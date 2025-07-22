782c6d951fd3df44475b4b79e06f006016137648
Bapu Pradhan <167701824+bapu6@users.noreply.github.com>
2025-07-22 12:02:15 +0530

    Update swagger-initializer.js

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -1,6 +1,18 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  const snippetTargets = [
+    { title: 'Python (requests)', target: 'python_requests' },
+    { title: 'cURL', target: 'shell_curl' },               // explicit cURL
+    { title: 'JavaScript (XHR)', target: 'javascript_xhr' },
+    { title: 'JavaScript (jQuery)', target: 'javascript_jquery' },
+    { title: 'Javascript (fetch)', target: 'javascript_fetch' },
+    { title: 'Java (OkHttp)', target: 'java_okhttp' },
+    { title: 'Java (Unirest)', target: 'java_unirest' },
+    { title: 'Go (native)', target: 'go_native' },
+    // Add more languages here...
+  ];
+
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url:"https://bapu6.github.io/swagger-doc/swagger.json",
@@ -11,8 +23,10 @@ window.onload = function() {
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
+      SwaggerUIBundle.plugins.DownloadUrl,
+      SwaggerSnippetGenerator(snippetTargets)
     ],
+    requestSnippetsEnabled: true,
     layout: "StandaloneLayout"
   });
 
